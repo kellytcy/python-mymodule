a3dabe68dee9120b81774c3b80819e536d79e708
Kelly Tan <kellytancheryong@gmail.com>
2022-03-27 15:08:12 +0800

    Added new function

--- mymodule.py
+++ mymodule.py
@@ -13,5 +13,15 @@ def get_weather(city):
     return weather["main"]["temp"] - 273.15
 
 
+def postal_lookup(postal_code):
+    sock = urlopen("http://api.postcodes.io/postcodes/" + postal_code)
+    result = sock.read()
+    sock.close()
+    details = json.loads(result)
+    return (details["result"]["latitude"], details["result"]["longitude"])
+
+
 if __name__ == "__main__":
     degrees = get_weather("OSLO")
+    location = postal_lookup("B323PP")
+    print(location)
